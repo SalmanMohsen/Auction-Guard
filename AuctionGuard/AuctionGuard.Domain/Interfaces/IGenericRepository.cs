3eea3a852baff759e75b825404ecec83307935de
Salman Mohsen <salman.mohsen88@gmail.com>
2025-07-14 11:53:02 +0300

    Implement repository design pattern
    Add a generic repository interface (IGenericRepository) and its implementations (AppGenericRepository and IdentityGenericRepository)

---
+++ AuctionGuard/AuctionGuard.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionGuard.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ AuctionGuard/AuctionGuard.Infrastructure/Repositories/IdentityGenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using AuctionGuard.Domain.Interfaces;
+using AuctionGuard.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionGuard.Infrastructure.Repositories
+{
+    public class IdentityGenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AuctionGuardIdentityDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public IdentityGenericRepository(AuctionGuardIdentityDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.FirstOrDefaultAsync(predicate);
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
